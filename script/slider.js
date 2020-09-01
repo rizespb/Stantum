f6fe4b16adda9eb1e1a0028f549698438449b17c
Alex <43300089+rizespb@users.noreply.github.com>
2020-09-01 16:04:41 +0300

    01.09.2020

--- script/slider.js
+++ script/slider.js
@@ -2,13 +2,18 @@ let UIController = (function() {
     
      // Создаем объект, в котором будем хранить все строки: в случае изменения имен классов мы можем изменить значение переменной в одном месте
     let DOMStrings = {
+
         sliderImageClass: '.slider__image',
         sliderImageClassActive: '.slider__image--active',
+        sliderMarkerClass: '.slider__marker',
+        sliderMarkerClassActive: '.slider__marker--active',
+
+
         leftBtn: '#slider-arrow-left',
         rightBtn: '#slider-arrow-right',
-        sliderMarker: '.slider__marker',
 
-        imageIDPrefix: 'sliderImage'
+        imageIDPrefix: 'sliderImage',
+        markerIDPrefix: 'sliderMarker'
     };
 
 
@@ -27,32 +32,47 @@ let controller = (function(UICtrl) {
     // Записываем в переменную DOM объект DOMStrings из UIController
     let DOM = UICtrl.getDOMStrings();
     
-    // ActiveImageID - переменная для хранения ID активного изображения
-    let activeImageID;
+    // ActiveImageID - переменная для хранения ID активного изображения и, соответственно, ID активного маркера
+    let activeImageID = 0;
 
     // Помещаем в переменную imageNodeList селекторы изображений слайдера
     let imagesNodeList = document.querySelectorAll(DOM.sliderImageClass);
 
+    // Переменная для хранения коллекции маркеров, которая создается в функции setupMarkers
+    let markersNodeList;
+
 
     // Добавляем первому изображению класс '.slider__image--active'
-    imagesNodeList[0].classList.add('slider__image--active');
+    imagesNodeList[activeImageID].classList.add('slider__image--active');
 
+    // ID активного изображения и, соответственно, ID его маркера
+    let activeID = 0;
 
-    // Добавляем маркеры изображений на страницу
+    // Получаем высоту контейнера, чтобы при переключении изображений высота не прыгала (не изменялась)
+    
+
+    ///// Добавляем маркеры изображений на страницу
     let setupMarkers = function(nodeList) {
 
         // Создаем переменную, в которой храним html-код добавляемого маркера
         let HTMLStringForMarker = '<span class="slider__marker" id="sliderMarker-%markerID%"></span>'
         
-        //в соответствии с количеством изображений (nodeList.length) добавляем необходимое количество маркеров, заменяя %markerID% на нужную цифру
+        // в соответствии с количеством изображений (nodeList.length) добавляем необходимое количество маркеров, заменяя %markerID% на нужную цифру
         for (let i = 0; i < nodeList.length; i++) {
             let newHTML = HTMLStringForMarker.replace('%markerID%', i);
             document.querySelector('.slider__markers-container').insertAdjacentHTML('beforeend', newHTML);
         };
 
         document.getElementById('sliderMarker-0').classList.add('slider__marker--active');
+
+
+        // Записываем п переменную markerNodeList коллекцию маркеров
+        markersNodeList = document.querySelectorAll(DOM.sliderMarkerClass);
     }
 
+
+
+    ///// Устанавливаем обработчики событий
     let setupEventListener = function() {
         document.querySelector(DOM.leftBtn).addEventListener('click', slideLeft);
         document.querySelector(DOM.rightBtn).addEventListener('click', slideRight);
@@ -60,46 +80,74 @@ let controller = (function(UICtrl) {
 
         // querySelectorAll возвращает коллекцию элементов nodeList. На коллекцию нельзя повесить обработчик событий. Поэтому мы коллекцию сохраняем в переменную MarkerNodeList. А затем с помощью цикла навешиваем на каждый элемент коллекции MarkerNodeList обработчик событий
         
-        let MarkerNodeList =  document.querySelectorAll('.slider__marker');
+        markersNodeList =  document.querySelectorAll('.slider__marker');
 
-        for (let i=0; i < MarkerNodeList.length; i++) {
-            MarkerNodeList[i].addEventListener('click', SliderMarkerControl);
+        for (let i=0; i < markersNodeList.length; i++) {
+            markersNodeList[i].addEventListener('click', sliderMarkerControl);
         }
     };
 
     
-
+    ///// Функция Стрелка ВЛЕВО - переключение изображения
     let slideLeft = function () {
-        activeImageID = parseInt(document.querySelector(DOM.sliderImageClassActive).id.split('-')[1]);
+        //activeImageID = parseInt(document.querySelector(DOM.sliderImageClassActive).id.split('-')[1]);
 
-        // Удаляем класс .slider__image--active у текущего изображения
+        // Удаляем класс .slider__image--active у текущего изображения и его маркера
         imagesNodeList[activeImageID].classList.remove('slider__image--active');
+        markersNodeList[activeImageID].classList.remove('slider__marker--active');
+
 
         if (activeImageID === 0) {
-            imagesNodeList[imagesNodeList.length - 1].classList.add('slider__image--active');
+            activeImageID = imagesNodeList.length - 1;
+
+            imagesNodeList[activeImageID].classList.add('slider__image--active');
+            markersNodeList[activeImageID].classList.add('slider__marker--active');
         } else {
-            imagesNodeList[activeImageID - 1].classList.add('slider__image--active');
+            activeImageID -= 1
+            imagesNodeList[activeImageID].classList.add('slider__image--active');
+            markersNodeList[activeImageID].classList.add('slider__marker--active');
         }
     };
 
+
+    ///// Функция Стрелка ВПРАВО - переключение изображения
     let slideRight = function () {
+        // let imageContainer = document.querySelector('.slider__image-container');
+        // let containerHeight = imageContainer.clientHeight;
+        // imageContainer.style.height = containerHeight + 'px';
 
-        // Получаем id элемента с классом .slider__image--active (sliderImageClassActive), отсекаем первую часть ID ('sliderImage-') и преобразуем в число, которое сохраняем в переменную activeImageID
-        activeImageID = parseInt(document.querySelector(DOM.sliderImageClassActive).id.split('-')[1]);
-
-        // Удаляем класс .slider__image--active у текущего изображения
+        // Удаляем класс .slider__image--active у текущего изображения и его маркера
         imagesNodeList[activeImageID].classList.remove('slider__image--active');
+        markersNodeList[activeImageID].classList.remove('slider__marker--active');
 
 
         if (activeImageID === imagesNodeList.length - 1) {
-            imagesNodeList[0].classList.add('slider__image--active');
+            activeImageID = 0;
+
+            imagesNodeList[activeImageID].classList.add('slider__image--active');
+            markersNodeList[activeImageID].classList.add('slider__marker--active');
         } else {
-            imagesNodeList[activeImageID + 1].classList.add('slider__image--active');
+            activeImageID = activeImageID + 1;
+
+            imagesNodeList[activeImageID].classList.add('slider__image--active');
+            markersNodeList[activeImageID].classList.add('slider__marker--active');
         }
+
+        //imageContainer.style.height = 'auto';
     }
 
-    let SliderMarkerControl = function() {
-        alert('Marker was clicked');
+
+    //// Функция переключения по МАРКЕРУ
+    let sliderMarkerControl = function(targetElement) {
+
+        markersNodeList[activeImageID].classList.remove('slider__marker--active');
+        imagesNodeList[activeImageID].classList.remove('slider__image--active');
+
+        // Записываем в переменную activeImageID ID выбранного маркера (числовое значение без префикса sliderMarker)
+        activeImageID = parseInt(targetElement.target.id.split('-')[1]);
+
+        imagesNodeList[activeImageID].classList.add('slider__image--active');
+        markersNodeList[activeImageID].classList.add('slider__marker--active');
     }
 
 
